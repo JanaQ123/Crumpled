669a2fba4644eb6b7683aacb4f8e50b4c4c970d3
Layan Tamimi <layanstamimi@gmail.com>
2026-05-18 15:35:02 +0300

    final audio level 3

---
+++ CrumpledGame/Assets/Scripts/Scripts_UI/LetterManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class LetterManager : MonoBehaviour
+{
+    public static bool[] collectedLetters = new bool[26];
+
+    public static int selectedLetter = -1;
+
+    public static void CollectLetter(int index)
+    {
+        collectedLetters[index] = true;
+    }
+}
---
+++ CrumpledGame/Assets/Scripts/Scripts_UI/LetterUI.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class LetterUI : MonoBehaviour
+{
+    public int letterIndex;
+
+    public GameObject unknownObject;
+    public GameObject foundObject;
+    public GameObject selectedObject;
+
+    // Right page
+    public GameObject letterView;
+
+    void Update()
+    {
+        RefreshState();
+    }
+
+    void RefreshState()
+    {
+        bool collected = LetterManager.collectedLetters[letterIndex];
+        bool selected = LetterManager.selectedLetter == letterIndex;
+
+        // NOT FOUND
+        if (!collected)
+        {
+            unknownObject.SetActive(true);
+            foundObject.SetActive(false);
+            selectedObject.SetActive(false);
+
+            if(letterView != null)
+                letterView.SetActive(false);
+
+            return;
+        }
+
+        // FOUND BUT NOT SELECTED
+        if (!selected)
+        {
+            unknownObject.SetActive(false);
+            foundObject.SetActive(true);
+            selectedObject.SetActive(false);
+
+            if(letterView != null)
+                letterView.SetActive(false);
+
+            return;
+        }
+
+        // SELECTED
+        unknownObject.SetActive(false);
+        foundObject.SetActive(false);
+        selectedObject.SetActive(true);
+
+        if(letterView != null)
+            letterView.SetActive(true);
+    }
+
+    public void SelectLetter()
+    {
+        if (!LetterManager.collectedLetters[letterIndex])
+            return;
+
+        LetterManager.selectedLetter = letterIndex;
+    }
+}
